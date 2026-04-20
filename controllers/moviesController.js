64c758b83cd0c89c909531219f54b7af39ec89dc
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-20 12:33:49 +0200

    add index logic content

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,9 +1,26 @@
+const connection = require("../data/db")
+
 const index = (req, res) => {
-  res.send('index')
+  
+    const sql= `SELECT * FROM movies`
+
+    connection.query(sql, (err, results)=>{
+        if(err) return res.status(500).json({
+            error:true,
+            message:"Database error"
+        })
+        
+        res.json(results)
+    })
+    
+
 }
 
 const show = (req, res) => {
   res.send('show')
 }
 
-module.exports = {index, show}
+module.exports = {
+    index,
+     show
+    }
